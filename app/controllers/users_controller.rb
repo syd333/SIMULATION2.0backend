d0279f5076deeb55af84af40d837b257fbc28671
syd333 <sydmiranda@icloud.com>
2021-04-09 10:51:26 -0700

    usercontroller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    
+    def getuser 
+        render json: {user: UserSerializer.new(current_user)}, status: :accepted
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def create 
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = issue_token({user_id: @user.id})
+            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
+        else 
+            render json: {error: 'failed to create user'}, status: :not_acceptable
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:email, :password)
+    end
+
 end
