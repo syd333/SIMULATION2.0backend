481709c8cb2e7b427bf146e3f478560a97cb726f
syd333 <sydmiranda@icloud.com>
2021-04-09 19:38:24 -0700

    configuring user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    # skip_before_action :authorized, only: [:login]
     
     def getuser 
         render json: {user: UserSerializer.new(current_user)}, status: :accepted
@@ -18,6 +19,16 @@ class UsersController < ApplicationController
             render json: {error: 'failed to create user'}, status: :not_acceptable
         end
     end
+
+    def login
+        user = User.find_by(username: params[:email])
+        if user && user.authenticate(params[:password])                                       
+          token = issue_token({user_id: user.id})
+          render json: {user: UserSerializer.new(user), jwt: token}, status: :accepted
+        else
+          render json: {error: 'That user could not be found'}, status: :unauthorized
+        end
+      end
 
     private
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :replies
   resources :users
+  post '/login', to: 'users#login'
+  post '/signup', to: 'users#create'
+  get '/getuser', to: 'users#getuser'
   resources :misses
+  resources :replies
   resources :favorites
 
 end
