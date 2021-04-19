c1b66a1a70b2eeef779d671532c86e80a328f0ba
syd333 <sydmiranda@icloud.com>
2021-04-19 15:16:39 -0700

    making commit computer going crazy

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,6 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def created_at
-    attributes['created_at'].strftime("%Y-%m-%d ")
+    attributes['created_at'].strftime("%m.%d.%Y ")
   end
 end
